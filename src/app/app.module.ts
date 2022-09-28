586d5e40aa32fe0e1bf4a8cac02c211819250a3c
Suelene Lima <suelene.lima@qintess.com>
2022-09-28 12:45:28 -0300

    Criação de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PessoaComponentComponent } from './components/pessoa-component/pessoa-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PessoaComponentComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/pessoa-component/pessoa-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoa-component',
+  templateUrl: './pessoa-component.component.html',
+  styleUrls: ['./pessoa-component.component.css']
+})
+export class PessoaComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
